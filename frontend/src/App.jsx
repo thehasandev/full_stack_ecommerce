5c41b9cc5af59937d6300791d40448adddfce119
Hasan <thehasandev@gmail.com>
2024-02-19 21:58:36 +0600

    flex component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Container from './components/Container'
+import Flex from './components/Flex'
 
 function App() {
   return (
     <Container>
+      <Flex>
       <h1>This is Tailwind</h1>
+      <h1>This is Tailwind</h1>
+
+      </Flex>
     </Container>
   )
 }
