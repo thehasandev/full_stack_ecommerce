a43b12e9fbf0c6509df6e9990e70c1dd30752bb8
Hasan <thehasandev@gmail.com>
2024-04-03 10:53:19 +0600

    add change in routing

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,11 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const route = require("./routes");
-const mongodbConfig = require("./config/mongodbConfig");
 const path = require("path");
 const http = require("http");
 const PORT = process.env.PORT || 4000;
 const server = http.createServer(app);
+const mongodbConfig = require("./config/mongodbConfig");
 
 // Middlewire Start
 app.use(cors());
@@ -16,7 +16,7 @@ app.use(route);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // Middlewire End
 
-server.listen(() => {
-  console.log(`Port is running to ${PORT}`);
-});
+server.listen(PORT,()=>{
+    console.log(`Port is connect to ${PORT}`);
+})
 mongodbConfig();
