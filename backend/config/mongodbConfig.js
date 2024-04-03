4af8772469b011b5c72102259386ce598fe199e0
Hasan <thehasandev@gmail.com>
2024-04-03 10:42:03 +0600

    add server

--- backend/config/mongodbConfig.js
+++ backend/config/mongodbConfig.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const mongodbConfig =()=>{
     mongoose.connect(`mongodb+srv://${process.env.DATABAE_USER_NAME}:${process.env.DATABASE_PASSWORD}@cluster0.px58yxr.mongodb.net/${process.env.DATABAE_NAME}?retryWrites=true&w=majority`)
-    .then(() => console.log('Connected!'))
+    .then(() => console.log('Database Connected!'))
 }
 
 module.exports = mongodbConfig
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,22 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const route = require("./routes")
-const mongodbConfig = require('./config/mongodbConfig')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const route = require("./routes");
+const mongodbConfig = require("./config/mongodbConfig");
 const path = require("path");
+const http = require("http");
+const PORT = process.env.PORT || 4000;
+const server = http.createServer(app);
 
-app.use(cors())
-app.use(express.json())
-mongodbConfig()
-
-app.use(route)
+// Middlewire Start
+app.use(cors());
+app.use(express.json());
+app.use(route);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.listen(8000,()=>{
-    console.log("Port is running");
-})
+// Middlewire End
+
+server.listen(() => {
+  console.log(`Port is running to ${PORT}`);
+});
+mongodbConfig();
