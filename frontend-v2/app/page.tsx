09678bf424ebef1ea458cf832fbf3b49e6784958
Hasan <thehasandev@gmail.com>
2024-05-12 13:33:22 +0600

    added a next js file and added rnext bootstrap

---
+++ frontend-v2/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "react-bootstrap";
+export default function Home() {
+  return (
+    <main>
+      <Button as="a" variant="primary">
+        Button as link
+      </Button>
+    </main>
+  );
+}
